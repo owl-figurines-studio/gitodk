7ce9d5c2748b84b63485ae75e34c2d089428d283
Author Name <Author Email>
2020-04-09 19:41:07 +0800

    优化代码,添加注释

--- odk/utils/base64.py
+++ odk/utils/base64.py
@@ -1,21 +1,31 @@
 import base64
 
 
-def base64_encode(all_path):
+def base64_encode(path):
+    """
+    将path进行编码加密
+    :param path: 需要加密的字符串
+    :return: 编码加密后的path
+    """
     # base64被编码的参数必须是二进制数据
-    byte_all_path = all_path.encode("utf-8")
+    byte_path = path.encode("utf-8")
     # 进行base64编码
-    based_byte_all_path = base64.b64encode(byte_all_path)
+    based_byte_path = base64.b64encode(byte_path)
     # 将二进制装换成str类型
-    based_str_all_path = based_byte_all_path.decode("utf-8")
+    based_str_all_path = based_byte_path.decode("utf-8")
     return based_str_all_path
 
 
-def base64_decode(based_all_path):
+def base64_decode(based_path):
+    """
+    将based_path进行解密
+    :param based_path:需要解密的字符串
+    :return: 解密后的字符串
+    """
     # base64被解码的参数必须是二进制数据
-    based_byte_all_path = based_all_path.encode("utf-8")
+    based_byte_path = based_path.encode("utf-8")
     # 进行base64解码
-    byte_all_path = base64.b64decode(based_byte_all_path)
+    byte_path = base64.b64decode(based_byte_path)
     # 将二进制装换成str类型
-    all_path = byte_all_path.decode("utf-8")
-    return all_path
+    path = byte_path.decode("utf-8")
+    return path
