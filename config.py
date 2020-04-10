b421cd516e8baec342a450085edf516369085fc4
Author Name <Author Email>
2020-04-10 15:17:21 +0800

    更新配置信息,添加celery

--- config.py
+++ config.py
@@ -1,9 +1,11 @@
 class Config(object):
-    '''
-    这是相关的配置
-    '''
+    """
+    配置信息
+    """
+    # 秘钥配置,随机乱码
     SECRET_KEY = 'JSFJBJBFEHI,NKHOHIO'
 
+    # redis配置
     REDIS_HOST = '127.0.0.1'
     REDIS_PORT = 6379
     REDIS_DB = 8
@@ -19,25 +21,13 @@ class Config(object):
     # 后台管理站点配置
     FLASK_ADMIN_SWATCH = 'cerulean'
 
-    # when backend is elasticsearch, MSEARCH_INDEX_NAME is unused
-    # flask-msearch will use table name as elasticsearch index name unless set __msearch_index__
-    MSEARCH_INDEX_NAME = 'msearch'
-    # simple,whoosh,elaticsearch, default is simple
-    MSEARCH_BACKEND = 'whoosh'
-    # table's primary key if you don't like to use id, or set __msearch_primary_key__ for special model
-    MSEARCH_PRIMARY_KEY = 'id'
-    # auto create or update index
-    MSEARCH_ENABLE = True
-    # SQLALCHEMY_TRACK_MODIFICATIONS must be set to True when msearch auto index is enabled
-    SQLALCHEMY_TRACK_MODIFICATIONS = True
-    # when backend is elasticsearch
-    ELASTICSEARCH = {"hosts": ["127.0.0.1.66:3306"]}
-
+    # celery配置
+    CELERY_BROKER_URL = 'redis://localhost:6379'
+    CELERY_RESULT_BACKEND = 'redis://localhost:6379'
 
 
 class DevelopConfig(Config):
     DEBUG = True
-
 
 
 class ProductionConfig(Config):
@@ -45,6 +35,6 @@ class ProductionConfig(Config):
 
 
 config = {
-    'dev':DevelopConfig,
-    'pro':ProductionConfig
+    'dev': DevelopConfig,
+    'pro': ProductionConfig
 }
--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-
+from celery import Celery
 
 from odk import create_app, db
 
@@ -8,12 +8,15 @@ from odk import create_app, db
 # 创建app
 app = create_app('dev')
 
+# 添加celery
+celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
+celery.conf.update(app.config)
 
+# 添加管理
 manager = Manager(app)
 
 # 数据库迁移管理
 Migrate(app, db)
-
 manager.add_command('db', MigrateCommand)
 
 
